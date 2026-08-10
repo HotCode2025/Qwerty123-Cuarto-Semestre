4217900deae9e3897f5fa6d70783ed9f02303cb9
GabrielSantarena <gabrielsantarena69@gmail.com>
2026-08-10 19:50:22 -0300

    1.3 Avanzamos con css y js
    #4

---
+++ JavaScript/E-Commerce/client/js/products.js
@@ -0,0 +1,30 @@
+const productos = [
+    {
+        id: 1,
+        productoName: "Banana",
+        price: 480,
+        quanty: 1,
+        img: "/client/media/banana.png",
+    },
+    {
+        id: 2,
+        productoName: "Leche",
+        price: 950,
+        quanty: 1,
+        img: "/client/media/leche.png",
+    },
+    {
+        id: 3,
+        productoName: "Pollo",
+        price: 750,
+        quanty: 1,
+        img: "/client/media/pollo.png",
+    },
+    {
+        id: 4,
+        productoName: "Mayonesa",
+        price: 510,
+        quanty: 1,
+        img: "/client/media/mayonesa.png",
+    },
+]
