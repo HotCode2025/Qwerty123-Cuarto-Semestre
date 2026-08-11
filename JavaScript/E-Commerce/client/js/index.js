500c1aa5b05e2760ceeaaf892a6665388a2895f3
santy171004 <santinmartin773@gmail.com>
2026-08-10 21:35:39 -0300

    1.4 Mostrando detalles finales
    #5

--- JavaScript/E-Commerce/client/js/index.js
+++ JavaScript/E-Commerce/client/js/index.js
@@ -1,11 +1,11 @@
 const shopContent = document.getElementById("shopContent");
 
-productos.fotEach((product) =>{
+productos.forEach((product) => {
     const content = document.createElement("div");
     content.innerHTML = `
-    <img src="${product.img}">
-    </h3>${product.productName}</h3>
-    <p>${product.price} $</p>
+        <img src="${product.img}" alt="${product.productName}">
+        <h3>${product.productName}</h3>
+        <p>${product.price} $</p>
     `;
-    shopContent.append(content);
+    shopContent.appendChild(content);
 });
