966d41cef2069a5ad18ece64925fc324cf181a6b
GabrielSantarena <gabrielsantarena69@gmail.com>
2026-08-17 19:44:00 -0300

    12.7 Comenzamos con las pruebas del Método -> agregarEstudiante() Parte 1 y 2 #21

--- Java/Clase 1/estudiante/src/main/java/UTN/datos/EstudianteDAO.java
+++ Java/Clase 1/estudiante/src/main/java/UTN/datos/EstudianteDAO.java
@@ -111,14 +111,22 @@ public class EstudianteDAO {
         List<Estudiante> estudiantes = estudianteDao.listarEstudiantes();
         estudiantes.forEach(System.out::println);// Función lambda para imprimir
 
-        // Buscar por id
-        var estudiante1 = new Estudiante(1);
-        System.out.println("Estudiantes antes de la busqueda: "+estudiante1);
-        var encontrado = estudianteDao.buscarEstudiantePorId(estudiante1);
-        if (encontrado)
-            System.out.println("Estudiante encontrado: "+estudiante1);
+        // Agregar estudiante
+        var nuevoEstudiante = new Estudiante("Carlos","Lara","5495544223","carlosl@mail.com");
+        var agregado = estudianteDao.agregarEstudiante(nuevoEstudiante);
+        if (agregado)
+            System.out.println("Estudiante agregado: "+nuevoEstudiante);
         else
-            System.out.println("No se encontro el estudiante: "+estudiante1.getIdEstudiante2022());
+            System.out.println("No se ha agregado estudiante: "+nuevoEstudiante);
+
+        // Buscar por id
+        //var estudiante1 = new Estudiante(1);
+        //System.out.println("Estudiantes antes de la busqueda: "+estudiante1);
+        //var encontrado = estudianteDao.buscarEstudiantePorId(estudiante1);
+        //if (encontrado)
+            //System.out.println("Estudiante encontrado: "+estudiante1);
+        //else
+            //System.out.println("No se encontro el estudiante: "+estudiante1.getIdEstudiante2022());
 
     }
 }
