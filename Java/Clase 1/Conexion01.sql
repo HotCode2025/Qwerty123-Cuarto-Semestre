239799a4df2d9b4c7a5ece63dd57497d9c64c8d0
AgusSantarena <santarenaagustin69@gmail.com>
2026-08-17 14:04:25 -0300

    Corrección en Conexion01.sql

--- Java/Clase 1/Conexion01.sql
+++ Java/Clase 1/Conexion01.sql
@@ -1,11 +1,11 @@
 -- Comenzamos con CRUD: create(insertar), read (leer), update(actualizar), delete(eliminar)
 -- Listar los estudiantes (read)
-SELECT * FROM estudiante2026;
+SELECT * FROM estudiante2022;
 -- Insertar estudiantes
-INSERT INTO estudiante2026(nombre,apellido,telefono,email) VALUES ("Juan", "Perez", "2614545456", "juan@mail.com");
+INSERT INTO estudiante2022(nombre,apellido,telefono,email) VALUES ("Juan", "Perez", "2614545456", "juan@mail.com");
 -- Update (modificar)
-UPDATE estudiante2026 SET nombre= "Juan Carlos", apellido= "Garcia" WHERE idestudiantes2026=1;
+UPDATE estudiante2022 SET nombre= "Juan Carlos", apellido= "Garcia" WHERE idestudiantes2022=1;
 -- Delete (eliminar)
-DELETE FROM estudiante2026 WHERE idestudiantes2026=5;
+DELETE FROM estudiante2022 WHERE idestudiantes2022=5;
 -- Para modificar el idestudiante2026 y comienze con 1
-ALTER TABLE estudiante2026 AUTO_INCREMENT = 1;
+ALTER TABLE estudiante2022 AUTO_INCREMENT = 1;
