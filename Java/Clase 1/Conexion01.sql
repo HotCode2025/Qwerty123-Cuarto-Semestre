b6b930e35915196fb1ca7da637625887ff1ae0cb
AgusSantarena <santarenaagustin69@gmail.com>
2026-08-18 16:08:28 -0300

    Correcion Clase01 parte 3
    Archivos modificados: Conexion01.sql y Conexion.java

--- Java/Clase 1/Conexion01.sql
+++ Java/Clase 1/Conexion01.sql
@@ -1,11 +1,11 @@
 -- Comenzamos con CRUD: create(insertar), read (leer), update(actualizar), delete(eliminar)
 -- Listar los estudiantes (read)
-SELECT * FROM estudiante2022;
+SELECT * FROM estudiantes2022;
 -- Insertar estudiantes
-INSERT INTO estudiante2022(nombre,apellido,telefono,email) VALUES ("Juan", "Perez", "2614545456", "juan@mail.com");
+INSERT INTO estudiantes2022(nombre,apellido,telefono,email) VALUES ("Juan", "Perez", "2614545456", "juan@mail.com");
 -- Update (modificar)
-UPDATE estudiante2022 SET nombre= "Juan Carlos", apellido= "Garcia" WHERE idestudiantes2022=1;
+UPDATE estudiantes2022 SET nombre= "Juan Carlos", apellido= "Garcia" WHERE idestudiantes2022=1;
 -- Delete (eliminar)
-DELETE FROM estudiante2022 WHERE idestudiantes2022=5;
+DELETE FROM estudiantes2022 WHERE idestudiantes2022=5;
 -- Para modificar el idestudiante2026 y comienze con 1
-ALTER TABLE estudiante2022 AUTO_INCREMENT = 1;
+ALTER TABLE estudiantes2022 AUTO_INCREMENT = 1;
--- Java/Clase 1/estudiante/src/main/java/UTN/conexion/Conexion.java
+++ Java/Clase 1/estudiante/src/main/java/UTN/conexion/Conexion.java
@@ -11,7 +11,7 @@ public class Conexion {
     public static Connection getConnection(){
         Connection conexion = null;
         //Variables para conectarnos a la base de datos
-        var baseDatos = "estudiantes2022";
+        var baseDatos = "estudiante";
         var url = "jdbc:mysql://localhost:3306/"+baseDatos;
         var usuario = "root";
         var password = "admin";
