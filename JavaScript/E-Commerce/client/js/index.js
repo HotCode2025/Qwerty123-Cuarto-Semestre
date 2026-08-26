0c3b2f368834d982e9afaae0f39f9970cd619da6
Fernando Adaro <feermza@gmail.com>
2026-08-25 23:38:03 -0300

    2.1 Código de botón de compra

--- JavaScript/E-Commerce/client/js/index.js
+++ JavaScript/E-Commerce/client/js/index.js
@@ -1,11 +1,29 @@
 const shopContent = document.getElementById("shopContent");
+const cart = []; //Este es nuestro carrito, un array vació
 
 productos.forEach((product) => {
     const content = document.createElement("div");
     content.innerHTML = `
-        <img src="${product.img}" alt="${product.productName}">
-        <h3>${product.productName}</h3>
-        <p>${product.price} $</p>
+        <img src="${product.img}" alt="${product.productoName}">
+        <h3>${product.productoName}</h3>
+        <p>$ ${product.price}</p>
     `;
     shopContent.appendChild(content);
+
+    const buyButton = document.createElement("button");
+    buyButton.innerText = "Comprar";
+
+    content.append(buyButton);
+
+    buyButton.addEventListener("click", ()=>{
+        cart.push({
+            id: product.id,
+            productName: product.productoName,
+            price: product.price,
+            quanty: product.quanty,
+            img: product.img,
+        })
+        console.log(cart)
+    })
+
 });
