e63fa3568da9e4e0c016513e225c64fb1b1cdf4a
Paul Romero <romeropaul24@gmail.com>
2026-08-27 22:31:11 -0300

    Issue 2.3- Carrito Header

--- JavaScript/E-Commerce/client/js/cart.js
+++ JavaScript/E-Commerce/client/js/cart.js
@@ -1,13 +1,32 @@
-const modalContainer = document.getElementById("modal-container");
-const modalOverlay = document.getElementById("modal-overlay");
+const modalContainer = document.getElementById("modal-container")
+const modalOverlay = document.getElementById("modal-overlay")
+
 
 const displayCart = () => {
-  //modal Header
-  const modalHeader = document.createElement("div");
+    modalContainer.innerHTML= "";
+    modalContainer.style.display ="block";
+    modalOverlay.style.display = "block";
 
-  const modalClose = document.createElement("div");
-  modalClose.innerText = "❌";
-  modalClose.className = "modal-close";
-  modalHeader.append(modalClose)
+    //modal Header
+    const modalHeader = document.createElement("div");
+    
+    const modalClose = document.createElement("div");
+    modalClose.innerText = "✖️"
+    modalClose.className = "modal-close";
+    modalHeader.append(modalClose)
 
-};
+    modalClose.addEventListener("click", () => {
+        modalContainer.style.display = "none",
+        modalOverlay.style.display = "none"
+    })
+
+    const modalTitle = document.createElement("div");
+    modalTitle.innerText = "Cart"
+    modalTitle.className = "modal-title"
+    modalHeader.append(modalTitle);
+
+    modalContainer.append(modalHeader);
+
+}
+
+cartBtn.addEventListener("click", displayCart)
