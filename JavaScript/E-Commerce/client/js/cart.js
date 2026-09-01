157cfd6c8cbb3082409d849a10d2bc50ab04a0f2
SSJesusChavez <jesus_albertochavez@outlook.com>
2026-08-31 21:56:19 -0300

    3.1 JS-- E-Commerce #51
    Línea 3: se agrega la constante cartBtn referencia al div #cart-btn 🛒 para poder engancharle el click que abre el modal del carrito.

--- JavaScript/E-Commerce/client/js/cart.js
+++ JavaScript/E-Commerce/client/js/cart.js
@@ -1,5 +1,6 @@
 const modalContainer = document.getElementById("modal-container")
 const modalOverlay = document.getElementById("modal-overlay")
+const cartBtn = document.getElementById("cart-btn")
 
 
 const displayCart = () => {
@@ -27,6 +28,27 @@ const displayCart = () => {
 
     modalContainer.append(modalHeader);
 
-}
+    //modal body
+    cart.forEach((product) => {
+        const modalContent = document.createElement("div");
+        modalContent.className = "modal-content";
+        modalContent.innerHTML = `
+        <div class="product-info">
+            <img class="product-img" src="${product.img}" />
+            <div class="product-info">
+                <h4>${product.productName}</h4>
+            </div>
+        </div>
+        <div class="quantity">
+            <span class="quantity-btn-decrease">-</span>
+            <span class="quantity-input">${product.quanty}</span>
+            <span class="quantity-btn-increase">+</span>
+        </div>
+        <div class="price">${product.price * product.quanty} $</div>
+        <div class="delete-product">❌</div>
+        `;
+        modalContainer.append(modalContent);
+    });
+};
 
 cartBtn.addEventListener("click", displayCart)
