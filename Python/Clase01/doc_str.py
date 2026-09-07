edc39206941158048bf582ecd2517d868439cb8f
Gustavo Ariel Rodriguez <garodrifornes79@gmail.com>
2026-09-07 20:07:12 -0300

    5.2 Dunder doc (__doc__) -> parte 1 y 2
    #79

--- Python/Clase01/doc_str.py
+++ Python/Clase01/doc_str.py
@@ -1,9 +1,10 @@
+from mi_clase import MiClase
 
+# help(MiClase)
 
-'''
-Podemos crear documentacion
-'''
+# print(MiClase.__doc__)
+# print(MiClase.__init__.__doc__)
+# print(MiClase.mi_metodo.__doc__)
+print(MiClase.mi_metodo)
+print(type(MiClase.mi_metodo))
 
-"""
-estas son las dos maneras de documentar
-"""
