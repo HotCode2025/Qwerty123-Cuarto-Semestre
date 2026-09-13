89fd5d0f07a0549b20be578468380e828c6adcaa
Fernando Adaro <feermza@gmail.com>
2026-09-13 18:22:31 -0300

    5.4 Método ejecutarOpciones, Parte 2 -> buscarEstudiantePorId y agregarEstudiante

--- Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -43,7 +43,7 @@ public class EstudiantesApplication implements CommandLineRunner {
 		logger.info(nl);
 		logger.info("""
 				******* Sistemas de Estudiantes *******
-				1. Listar Estudainte
+				1. Listar Estudiante
 				2. Buscar Estudiante
 				3. Agregar Estudiante
 				4. Modificar Estudiante
@@ -57,9 +57,42 @@ public class EstudiantesApplication implements CommandLineRunner {
 
 		switch (opcion) {
 			case 1 -> { // Listar estudiantes
-				logger.info(nl + "Listado de estudiantes:");
+				logger.info(nl + "Listado de estudiantes: "+nl);
 				List<Estudiantes2022> estudiantes = estudianteServicio.listarEstudiantes();
-				estudiantes.forEach(estudiante -> logger.info(nl + estudiante));
+				estudiantes.forEach((estudiante -> logger.info(estudiante.toString()+nl)));
+			}
+			case 2 -> { //Buscar estudainte por id
+				logger.info("Digite el id estudiante a buscar: ");
+				var idEstudiante = Integer.parseInt(consola.nextLine());
+				Estudiantes2022 estudiante = estudianteServicio.buscarEstudiantePorId(idEstudiante);
+				if (estudiante != null)
+					logger.info("Estudiante encontrado: "+ estudiante + nl);
+				else
+					logger.info("Estudiante NO encontrado: " + estudiante +nl);
+			}
+
+			case 3 -> {// Agregar estudiante
+				logger.info("Agregar estudiante: "+nl);
+				logger.info("Nombre: ");
+				var nombre = consola.nextLine();
+				logger.info("Apellido: ");
+				var apellido = consola.nextLine();
+				logger.info("Teléfono: ");
+				var telefono = consola.nextLine();
+				logger.info("Email: ");
+				var email = consola.nextLine();
+				// Crear el objeto estudiante sin el id
+				var estudiante = new Estudiantes2022();
+				estudiante.setNombre(nombre);
+				estudiante.setApellido(apellido);
+				estudiante.setTelefono(telefono);
+				estudiante.setEmail(email);
+				estudianteServicio.guardarEstudiante(estudiante);
+				logger.info("Estudiante agregado: "+estudiante+nl);
+				
+
+
+
 			}
 		}
 
