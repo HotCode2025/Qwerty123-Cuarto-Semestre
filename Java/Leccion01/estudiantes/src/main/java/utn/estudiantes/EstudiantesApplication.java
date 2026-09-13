079aa81e86a92c851a6e1de078ad98898ed616d9
Gustavo Contreras <gustavocontreras2022@gmail.com>
2026-09-13 10:56:16 -0300

    5.4 Método ejecutarOpciones Parte 1 - Listar estudiantes #97

--- Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
+++ Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/EstudiantesApplication.java
@@ -51,4 +51,18 @@ public class EstudiantesApplication implements CommandLineRunner {
 				6. Salir
 				Eliga una opción:""");
 	}
+	private boolean ejecutarOpciones(Scanner consola) {
+		var opcion = Integer.parseInt(consola.nextLine());
+		var salir = false;
+
+		switch (opcion) {
+			case 1 -> { // Listar estudiantes
+				logger.info(nl + "Listado de estudiantes:");
+				List<Estudiantes2022> estudiantes = estudianteServicio.listarEstudiantes();
+				estudiantes.forEach(estudiante -> logger.info(nl + estudiante));
+			}
+		}
+
+		return salir;
+	}
 }
