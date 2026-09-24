8131c9e26a45975f50dc3a6bc6e0e711a0c1e9c5
Nahuel Spikerman <nahuelspikerman@gmail.com>
2026-09-24 17:07:10 -0300

    Node - 6.2 Pruebas para aclarar conceptos y 6.3 Buenas prácticas en Node

--- Node/01-Hola-Mundo/entorno.js
+++ Node/01-Hola-Mundo/entorno.js
@@ -1,3 +1,6 @@
-let nombre = process.env.NOMBRE;
+
+let nombre = process.env.NOMBRE || 'Sin nomrbe';
+let web = process.env.MI_WEB || 'No tengo web';
 
 console.log('Hola '+nombre);
+console.log('Mi web es: '+web);
